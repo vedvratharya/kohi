64df2ad2cbf47ee7e0fdd3f86ab3e69237dae42e
Ved Arya <vedvratharya17@gmail.com>
2025-02-16 16:10:25 +0530

    dll to exe for testbed bat

--- testbed/build.bat
+++ testbed/build.bat
@@ -13,9 +13,9 @@ REM echo "Files: " %cFilenames%
 SET assembly=testbed
 SET compilerFlags=-g
 REM -Wall -Werror
-SET includeFlags=-Isrc -I../engine/src
+SET includeFlags=-Isrc -I../engine/src/
 SET linkerFlags=-L../bin/ -lengine.lib
 SET defines=-D_DEBUG -DKIMPORT
 
 echo "Building %assembly%%..."
-clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
+clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
